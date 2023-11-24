a0954d20bed51e79134f8b767c6c6d9094390914
Michael Schartner <michael.m.schartner@gmail.com>
2023-11-24 12:29:45 +0000

    Update granger.py

--- granger.py
+++ granger.py
@@ -3,20 +3,29 @@ from collections import Counter
 from pathlib import Path
 from spectral_connectivity import Multitaper, Connectivity
 import time
+from itertools import product
 
-from brainwidemap import load_good_units
+from brainwidemap import load_good_units, bwm_query
 from iblutil.numerical import bincount2D
 from one.api import ONE
 from iblatlas.regions import BrainRegions
+import seaborn as sns
+import pandas as pd
+
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-
+import gc as garbage
 
 one = ONE(base_url='https://openalyx.internationalbrainlab.org',
           password='international', silent=True)
 br = BrainRegions()
-T_BIN=0.0125
+T_BIN = 0.0125
+
+# save results here
+pth_res = Path(one.cache_dir, 'granger', 'res')
+pth_res.mkdir(parents=True, exist_ok=True)
+
 
 def get_allen_info():
     r = np.load(Path(one.cache_dir, 'dmn', 'alleninfo.npy'),
@@ -29,7 +38,7 @@ def bin_average_neural(eid, mapping='Beryl', nmin=1):
     bin neural activity; bin, then average firing rates per region
     from both probes if available
     
-    used to get session wide time series, not cut into trials
+    used to get session-wide time series, not cut into trials
     '''
     
     pids, probes = one.eid2pid(eid)
@@ -39,8 +48,8 @@ def bin_average_neural(eid, mapping='Beryl', nmin=1):
         R, times, _ = bincount2D(spikes['times'], spikes['clusters'], T_BIN)
         acs = br.id2acronym(clusters['atlas_id'], mapping=mapping)
         regs = Counter(acs)
-        regs2 = [x for x in regs if 
-                 ((regs[x] >= nmin) and (x not in ['root','void']))]
+        regs2 = {x: regs[x] for x in regs if 
+                 ((regs[x] >= nmin) and (x not in ['root','void']))}
                  
         R2 = np.array([np.mean(R[acs == reg],axis=0) for reg in regs2])
           
@@ -118,12 +127,13 @@ def gc(r, segl=10):
     return psg, c.frequencies    
 
 
-def plot_gc(eid, segl=10, show_fig=True):
+def get_gc(eid, segl=10, show_fig=False):
 
     '''
-    For all regions, plot example segment time series, psd,
+    For all regions, plot example segment time series, psg,
     matrix for max gc across freqs
     '''
+    
     time00 = time.perf_counter()
     
        
@@ -191,11 +201,11 @@ def plot_gc(eid, segl=10, show_fig=True):
                       
         axs[2].set_xticks(np.arange(m.shape[0]))
         axs[2].set_xticklabels(regs, rotation=90)
-        axs[2].set_xlabel('source')
+        axs[2].set_ylabel('source')
         axs[2].set_yticks(np.arange(m.shape[1]))
         axs[2].set_yticklabels(regs)
         axs[2].set_xlabel('target')     
-        axs[2].set_title('max GC')
+        axs[2].set_title('max GC across freqs')
                       
         cb = plt.colorbar(ims,fraction=0.046, pad=0.04)              
     
@@ -205,8 +215,122 @@ def plot_gc(eid, segl=10, show_fig=True):
 
            
     else:
-        return m, regs                   
+        return regsd, psg, freqs                  
     
+
+def get_all_granger(eids='all'):
+
+    '''
+    get spectral directed granger for all bwm sessions
+    '''
+
+    if isinstance(eids, str):
+        df = bwm_query(one)
+        eids = np.unique(df[['eid']].values)
+                
+    Fs = []
+    k = 0
+    print(f'Processing {len(eids)} sessions')
+    time0 = time.perf_counter()
+    for eid in eids:
+               
+        try:
+
+            regsd, psg, freqs = get_gc(eid)
+            
+            D = {'regsd': regsd,
+                 'psg': psg,
+                 'freqs': freqs}
+                 
+            np.save(Path(pth_res, f'{eid}.npy'), D, 
+                    allow_pickle=True)
+
+            garbage.collect()
+            print(k + 1, 'of', len(eids), 'ok')
+            
+        except BaseException:
+            Fs.append(eid)
+            garbage.collect()
+            print(k + 1, 'of', len(eids), 'fail', eid)
+
+        k += 1    
+
+    time1 = time.perf_counter()
+    print(time1 - time0, f'sec for {len(eids)} sessions')
+    print(len(Fs), 'failures')
+    return Fs
+
+
+
+def plot_res(nmin=10, sessmin = 4):
+
+    '''
+    Group and plot granger results
+    
+    nmin: minimum number of neurons per region to be included
+    sessmin: min number of sessions with region combi
+    '''
+    
+    p = pth_res.glob('**/*')
+    files = [x for x in p if x.is_file()]
+    
+    d = {}
+    nd = []
+    for sess in files:    
+        D = np.load(sess, allow_pickle=True).flat[0]
+        m = np.max(D['psg'], axis=0)
+        regs = list(D['regsd'])
+        
+        if not isinstance(D['regsd'], dict):
+            nd.append(sess)
+            continue
+        
+        
+        for i in range(len(regs)):
+            for j in range(len(regs)):
+                if i == j:
+                    continue
+                
+                if ((D['regsd'][regs[i]] < nmin) or
+                    (D['regsd'][regs[j]] < nmin)):
+                    continue
+                
+                if f'{regs[i]}__{regs[j]}' in d:
+                    d[f'{regs[i]}__{regs[j]}'].append(m[i, j])
+                else:
+                    d[f'{regs[i]}__{regs[j]}'] = []
+                    d[f'{regs[i]}__{regs[j]}'].append(m[i, j])    
+
+
+    dm = {x: np.mean(d[x]) for x in d if len(d[x]) > sessmin}
+    dm_sorted = dict(sorted(dm.items(), key=lambda item: item[1]))
+
+    exs = np.concatenate([list(dm_sorted.keys())[:10],
+                          list(dm_sorted.keys())[-10:]])
+    
+    d_exs = {x:d[x] for x in exs}
+
+    fig, ax = plt.subplots(figsize=(5,4))
+    sns.stripplot(data=d_exs, ax=ax)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+    ax.set_title(f'10 min and 10 max directed region pairs \n'
+                 f' with at least {sessmin} sessions out of all'
+                 f' {len(files)} sessions \n'
+                 f'only regs with at least {nmin} neurons'
+                 f' \n one dot per session')
+    ax.set_ylabel('max directed spectral Granger across freqs')
+    fig.tight_layout()
+
+    return nd
+
+
+
+
+
+
+
+
+
 
 
 
